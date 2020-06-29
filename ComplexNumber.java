427a2f1e6d3275a9a11f8e29071fa1a53ef36216
infinite-void <srinathsk18@gmail.com>
2020-06-29 20:48:47 +0530

    complete java ComplexNumber

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -3,9 +3,26 @@ public class ComplexNumber {
 
     void Print() {
         if(imag > -1)
-               System.out.println(real + "+" + imag + "i");
+               System.out.println(real + " +" + imag + "i");
         else 
                 System.out.println(real + " " + imag + "i");
     }
 
+    void PrintConjugate() {
+            System.out.print(real + " ");
+            if(imag <= 0)
+                System.out.println("+" + -1 * imag + "i");
+            else
+                System.out.println(-1 * imag + "i");
+    }
+
+    void ApplyConjugate() {
+        imag = -1 * imag;
+    }
+
+    double Magnitude() {
+        return Math.sqrt(real * real + imag * imag);
+    }
 }
+
+//github.com/infinite-void/oops-concepts
--- Main.java
+++ Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         ComplexNumber a = new ComplexNumber();
         a.real = 3;
-        a.imag = 2;
+        a.imag = 0;
         a.Print();
+        a.PrintConjugate();
+        a.ApplyConjugate();
+        a.Print();
+        a.PrintConjugate();
+        System.out.println(a.Magnitude());
     }
 }
